eb9bcf9ba2ecd3f29c3c49fa189b59f15f0ba026
AdebayoBunmiSolomon <hardeybayorsamuel11@gmail.com>
2025-07-01 07:04:50 -0700

    bundled for native ios folder and tested

--- App.tsx
+++ App.tsx
@@ -74,7 +74,7 @@ export default function App() {
           {!isFontLoadingComplete ? (
             <AppLoader />
           ) : (
-            <Router isAuthenticated={true} />
+            <Router isAuthenticated={false} />
           )}
         </SafeAreaProvider>
       </PersistQueryClientProvider>
