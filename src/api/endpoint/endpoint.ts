2adbb9e04755c45c6d9f22768bef67df92930ccb
Adebayo Bunmi Solomon <61608290+AdebayoBunmiSolomon@users.noreply.github.com>
2025-08-31 14:26:53 +0100

    removed filter logic in get all servcies

--- src/api/endpoint/endpoint.ts
+++ src/api/endpoint/endpoint.ts
@@ -10,7 +10,7 @@ export const endpoint = {
   APP: {
     getCategories: "/category", //get category
     scheduleConsultation: "/consultations",
-    getAllService: "/services?page=1&per_page=10&sort=desc", //&search=&type=&range%5Bfrom%5D=&range%5Bto%5D=
+    getAllService: "/services", //&search=&type=&range%5Bfrom%5D=&range%5Bto%5D=
     viewService: "/services", //pass the service_uuid as query parameter
     sendMessage: "/admin/chats",
     getSettings: "/settings",
